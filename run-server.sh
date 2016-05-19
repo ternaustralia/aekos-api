5808cc5fbe42a5e69477ea549b278fcbe92daca9
Tom Saleeba <tom.saleeba@gmail.com>
2016-05-19 15:13:57 +0930

    changed the run-server script to use the spring-boot maven plugin so it supports faster dev work

--- run-server.sh
+++ run-server.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 cd `dirname $0`
-trap "echo Exited!; exit;" SIGINT SIGTERM
-mvn clean package
-MVN_RC=$?
-JAR_FILE=`find target/ -name "aekos-api-*.jar"`
-if [ "$MVN_RC" != "0" ]; then
-  echo "ERROR: maven didn't succeed. Refusing to start server"
-  exit $MVN_RC
-fi
-java -Dserver.port=8099 -jar "$JAR_FILE"
+mvn -Dserver.port=8099 spring-boot:run
