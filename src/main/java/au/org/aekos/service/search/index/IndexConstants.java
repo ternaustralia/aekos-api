0094ad306ee0b6161448fad8f59e522b9aadacc1
Tillbilly <madbatch@gmail.com>
2016-06-05 17:28:28 +0930

    uncommented PropertySource

---
+++ src/main/java/au/org/aekos/service/search/index/IndexConstants.java
@@ -0,0 +1,11 @@
+package au.org.aekos.service.search.index;
+
+public class IndexConstants {
+
+	public static final String TRAIT_VALUE = "traitValue";
+	public static final String DISPLAY_VALUE = "displayValue";
+	public static final String SEARCH = "search";
+	public static final String SEARCH_LEV = "search_lev";
+	public static final String SEARCH_SUB= "search_sub";
+	
+}
