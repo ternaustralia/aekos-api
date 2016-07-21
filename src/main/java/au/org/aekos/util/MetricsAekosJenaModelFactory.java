1f0146c682d159f4e37bc487abadb42a393d6114
Andrew Tokmakoff <tokmakoff@computer.org>
2016-07-21 21:36:10 +1000

    Added some trace to metrics store on startup, like the auth store.

--- src/main/java/au/org/aekos/util/MetricsAekosJenaModelFactory.java
+++ src/main/java/au/org/aekos/util/MetricsAekosJenaModelFactory.java
@@ -1,11 +1,15 @@
 package au.org.aekos.util;
 
 import org.apache.jena.rdf.model.Model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MetricsAekosJenaModelFactory extends AbstractAekosJenaModelFactory {
+
+	private static final Logger logger = LoggerFactory.getLogger(MetricsAekosJenaModelFactory.class);
 
 	@Value("${aekos-api.metrics-model-name}")
 	private String modelName;
@@ -20,11 +24,15 @@ public class MetricsAekosJenaModelFactory extends AbstractAekosJenaModelFactory 
 	
 	@Override
 	void doPostConstructStats(Model newInstance) {
-		// do nothing
+		countExistingMetrics(newInstance);
 	}
 
 	@Override
 	String getModelPath() {
 		return modelPath;
+	}
+	
+	private void countExistingMetrics(Model newInstance) {
+		logger.info(getModelName() + " metricRecord store has " + newInstance.listSubjects().toList().size() + " existing metric(s).");
 	}
 }
