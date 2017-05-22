6ccc86484ca8499c4e5898225a71aef3fd5502d4
Tom Saleeba <tom.saleeba@gmail.com>
2017-05-22 11:40:28 +0930

    added a script to get the swagger def from API Gateway

---
+++ aekos-api-serverless/get-swagger.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Downloads the current Swagger definition from AWS API Gateway
+cd `dirname $0`
+API_ID=ouzan2n47i
+STAGE=dev
+FILE_TYPE=json
+REGION=us-west-1
+OUTPUT=swagger-aekos-api-$STAGE.$FILE_TYPE
+printf "Downloading Swagger definition to $OUTPUT
+  API ID: $API_ID
+   Stage: $STAGE
+  Accept: $FILE_TYPE
+  Region: $REGION\n\n"
+
+aws apigateway get-export \
+  --rest-api-id=$API_ID \
+  --stage-name=$STAGE \
+  --export-type=swagger \
+  --accept=application/$FILE_TYPE \
+  --region=$REGION \
+  $OUTPUT
