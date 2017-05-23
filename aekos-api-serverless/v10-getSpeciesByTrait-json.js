5fad2837054e34a0eecea44f269150df38b35fc6
Tom Saleeba <tom.saleeba@gmail.com>
2017-05-23 16:35:11 +0930

    added the species by trait function

---
+++ aekos-api-serverless/v10-getSpeciesByTrait-json.js
@@ -0,0 +1,30 @@
+'use strict'
+let r = require('./response-helper')
+let db = require('./db-helper')
+const traitNameParam = 'traitName'
+
+module.exports.handler = (event, context, callback) => {
+  // FIXME get repeated query string params mapping correctly rather than just the last one
+  if (!r.isQueryStringParamPresent(event, traitNameParam)) {
+    r.badRequest(callback, `the '${traitNameParam}' query string parameter must be supplied`)
+    return
+  }
+  // FIXME handle escaping a list when we can get multiple names
+  let traitName = event.queryStringParameters[traitNameParam]
+  let escapedTraitName = db.escape(traitName)
+  let pageSize = r.getOptionalParam(event, 'pageSize', 50)
+  let pageNum = r.getOptionalParam(event, 'pageNum', 1)
+  let offset = r.calculateOffset(pageNum, pageSize)
+  const sql = `
+    SELECT COALESCE(s.scientificName, s.taxonRemarks) AS name, count(*) AS recordsHeld, 'notusedanymore' AS id
+    FROM species AS s
+    INNER JOIN traits AS t
+    ON t.parentId = s.id
+    AND t.traitName in (${escapedTraitName})
+    GROUP BY 1
+    ORDER BY 1
+    LIMIT ${pageSize} OFFSET ${offset};`
+  db.execSelect(sql, (queryResult) => {
+    r.ok(callback, queryResult)
+  })
+}
