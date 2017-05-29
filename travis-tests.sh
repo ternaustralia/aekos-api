9fcf9fa351229d8fd0a6dbeb1873d454d9474785
Tom Saleeba <tom.saleeba@gmail.com>
2017-05-29 18:35:41 +0930

    let's try using the node image for travis

--- travis-tests.sh
+++ travis-tests.sh
@@ -2,8 +2,8 @@
 set -ev
 cd `dirname $0`
 pushd aekos-api-producer
-mvn clean test
+./mvnw clean test
 popd
 cd aekos-api-serverless
-npm install # Java VM in Travis doesn't have yarn
-npm --harmony test
+yarn
+npm test
