5bab67278405408a980c3d3d1fe06fe7aa5cc2ab
Tom Saleeba <tom.saleeba@gmail.com>
2017-05-29 18:55:14 +0930

    let's install nvm then too

--- travis-tests.sh
+++ travis-tests.sh
@@ -5,6 +5,7 @@ pushd aekos-api-producer
 ./mvnw clean test
 popd
 cd aekos-api-serverless
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 nvm install 7.10
 nvm use 7.10
 npm install -g yarn
