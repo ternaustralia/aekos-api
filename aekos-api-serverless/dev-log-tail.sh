cabcbc4bda4288907d15fa5e4110bcdb53eb9c92
Tom Saleeba <tom.saleeba@gmail.com>
2017-06-20 12:18:04 +0930

    updated the help messages to reflect the new versioning scheme

--- aekos-api-serverless/dev-log-tail.sh
+++ aekos-api-serverless/dev-log-tail.sh
@@ -5,7 +5,7 @@ if [ -z "$FUNC" ]; then
   echo "Tails to log of the supplied function."
   echo "The function name is one of the keys under 'functions' in serverless.yml."
   echo "Usage: $0 <function-name>"
-  echo "   eg: $0 v10-getTraitVocab-json"
+  echo "   eg: $0 v1-getTraitVocab-json"
   exit 1
 fi
 FUNC=`bash -c "echo $FUNC | sed 's/\.js$//'"`
--- aekos-api-serverless/dev-update-func.sh
+++ aekos-api-serverless/dev-update-func.sh
@@ -5,7 +5,7 @@ if [ -z "$FUNC" ]; then
   echo "Updates a single function (much quicker than a full deploy)."
   echo "The function name is one of the keys under 'functions' in serverless.yml."
   echo "Usage: $0 <function-name>"
-  echo "   eg: $0 v10-getTraitVocab-json"
+  echo "   eg: $0 v1-getTraitVocab-json"
   exit 1
 fi
 FUNC=`bash -c "echo $FUNC | sed 's/\.\(j\|t\)s$//'"`
