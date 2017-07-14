436325ab1ca632ab042af2c4382d646e78098f35
Tom Saleeba <tom.saleeba@gmail.com>
2017-07-14 18:34:09 +0930

    first try at loading serverless variables from a .js file, added a test to assert conformance to a JSON schema

--- aekos-api-serverless/v1-getEnvironmentalVariableVocab-json.js
+++ aekos-api-serverless/v1-getEnvironmentalVariableVocab-json.js
@@ -34,3 +34,31 @@ function mapQueryResult (queryResult) {
   })
   return queryResult
 }
+
+module.exports.responseSchema = () => {
+  return {
+    '$schema': 'http://json-schema.org/draft-04/schema#',
+    'additionalItems': false,
+    'items': {
+      'additionalProperties': false,
+      'properties': {
+        'code': {
+          'type': 'string'
+        },
+        'label': {
+          'type': 'string'
+        },
+        'recordsHeld': {
+          'type': 'integer'
+        }
+      },
+      'required': [
+        'recordsHeld',
+        'code',
+        'label'
+      ],
+      'type': 'object'
+    },
+    'type': 'array'
+  }
+}
