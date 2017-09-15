3d6b5fc8de9c5fea01c972e58cbcdd156f097753
Tom Saleeba <tom.saleeba@gmail.com>
2017-09-15 19:06:43 +0930

    fix: update delete script to use the new stack name suffix

--- aekos-api-swagger-site/cloudformation/delete-stack.sh
+++ aekos-api-swagger-site/cloudformation/delete-stack.sh
@@ -2,14 +2,15 @@
 # Deletes an AWS stack
 cd `dirname $0`
 set -e
-stackname=$1
 source ./src/create-stack.sh
-if [ -z "$stackname" ]; then
+stackprefix=$1
+if [ -z "$stackprefix" ]; then
   echo "[ERROR] no stack name provided"
   echo "usage: $0 [stack name]"
   echo "   eg: $0 dev"
   exit 1
 fi
+stackname="$stackprefix-api-site"
 echo "[INFO] deleting stack '$stackname'"
 aws --region=$REGION \
   cloudformation \
@@ -17,7 +18,7 @@ aws --region=$REGION \
   --stack-name $stackname
 echo "[WARN] this will fail if you haven't emptied all the S3 buckets that are to be deleted"
 sleep 5
-echo "[INFO] checking progress"
+echo "[INFO] checking progress (this might say this id does not exist if it deletes too quickly)"
 aws --region=$REGION \
   cloudformation \
   describe-stacks \
